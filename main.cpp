5b24d6aa32ad9275bbb949e10fce566abb893b85
Григорий Динмухаметов <warface_grixan@mail.ru>
2025-09-17 10:00:22 +0300

    интеграция функции sumGPAs в меню

--- main.cpp
+++ main.cpp
@@ -51,6 +51,7 @@ int main() {
         std::cout << "Меню:\n";
         std::cout << "1. Добавить студента\n";
         std::cout << "2. Вывести список студентов\n";
+        std::cout << "3. Показать сумму средних баллов\n";
         std::cout << "0. Выход\n";
         std::cout << "Выберите действие: ";
         std::cin >> choice;
@@ -61,6 +62,9 @@ int main() {
                 break;
             case 2:
                 displayStudents(database);
+                break;
+            case 3:
+                std::cout << "Сумма средних баллов: " << sumGPAs(database) << "\n";
                 break;
             case 0:
                 std::cout << "Выход из программы.\n";
